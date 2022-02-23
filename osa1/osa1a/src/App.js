c6d0b36440425488c514bbe34f2fffbb2e088b70
Topias Laatu <topias.laatu@gmail.com>
2022-02-23 17:13:08 +0200

    osa1b

--- osa1/osa1a/src/App.js
+++ osa1/osa1a/src/App.js
@@ -1,12 +1,11 @@
 const Header = (props) =>
   <h1>{props.course}</h1>
 
-const Content = (props) => 
+const Part = (props) => 
   <p>{props.part} {props.exercises}</p>
 
 const Total = (props) =>
   <p>Number of exercises {props.total}</p>
-
 
 function App() {
   const part1 = 'Fundamentals of React'
@@ -16,13 +15,25 @@ function App() {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const p1 = <Part part={part1} exercises={exercises1}/>
+  const p2 = <Part part={part2} exercises={exercises2}/>
+  const p3 = <Part part={part3} exercises={exercises3}/>
+
+  const Content = () => {
+    return (
+      <div>
+        {p1}
+        {p2}
+        {p3}
+      </div>
+    )
+  }
+
   return (
     <div>
       <Header course='Half Stack application development' />
 
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content />
 
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
