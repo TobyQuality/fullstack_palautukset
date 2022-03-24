b429ce8ae487a2806ef71fbc24e3725aa6fac0a6
Topias Laatu <topias.laatu@gmail.com>
2022-03-25 01:23:47 +0200

    osa2.3

---
+++ osa2/src/components/Total.js
@@ -0,0 +1,11 @@
+const Total = ( {parts }) => {
+    
+    const total = parts.reduce((sum, part) => {
+        console.log('Iteration:', sum, part)
+        return sum + part.exercises 
+    }, 0)
+
+    return (<h4>total of {total} exercises</h4>)
+}
+
+export default Total
