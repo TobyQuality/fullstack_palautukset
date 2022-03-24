884ece83cd46c9217ca4e22e6eef41b463329709
Topias Laatu <topias.laatu@gmail.com>
2022-03-24 20:04:42 +0200

    osa2.2

--- osa2/src/components/Course.js
+++ osa2/src/components/Course.js
@@ -1,11 +1,20 @@
 import Content from './Content'
 import Header from './Header'
 
+const Total = ({parts}) => {
+    let totalNumberOfExercises = 0
+    parts.map((part) => 
+        totalNumberOfExercises += part.exercises 
+    )
+    return (<h3>total of {totalNumberOfExercises} exercises</h3>)
+}
+
 const Course = ({ course }) => {
     return(
         <div>
             <Header title={course.name} />
             <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
         </div>
     )
 }
