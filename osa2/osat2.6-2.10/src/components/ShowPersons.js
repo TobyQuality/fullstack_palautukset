bd56517a5bb99c4dcb900556534bb3e44110719b
Topias Laatu <topias.laatu@gmail.com>
2022-03-30 14:16:56 +0300

    kaikki tehtavat suoritettu

---
+++ osa2/osat2.6-2.10/src/components/ShowPersons.js
@@ -0,0 +1,13 @@
+//saa App-tasolta propsina puhelinluettelon listan persons ja filtteröintiä varten merkkijonon newFilter
+const ShowPersons = ({ persons, newFilter }) => {
+    const filteredList = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    return (
+      <div>
+        {filteredList.map( (person) => 
+          <p key={person.name}>{person.name} {person.number}</p>
+        )}
+      </div>
+    )
+  }
+  
+  export default ShowPersons
