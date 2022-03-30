d6857eb1b4db0066979924052ca195f00e66f6cd
Topias Laatu <topias.laatu@gmail.com>
2022-03-30 14:01:55 +0300

    osa2.10

--- osa2/osat2.6-2.10/src/App.js
+++ osa2/osat2.6-2.10/src/App.js
@@ -1,39 +1,31 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import ShowPersons from './components/ShowPersons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
-  const submitName = (e) => {
-    e.preventDefault()
-    setPersons(persons.concat({name: newName}))
-    setNewName('')
-    console.log(persons)
-  }
-
-  const ShowPersons = ( {persons} ) => {
-    return (persons.map((person) => {
-      return(<p key={person.name}> {person.name} </p>)
-    }))
-  }
+  //käärin tähän muuttujaan propseina välitettävät arvot, joita tarvitaan PersonForm-komponentissa
+  const propslist = {persons, setPersons, newName, setNewName, newNumber, setNewNumber}
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={submitName}>
-        <div>
-          name: <input value={newName} onChange={(e) => {setNewName(e.target.value)}}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter newFilter={newFilter} setNewFilter={setNewFilter} />
+      <h2>add a new</h2>
+      <PersonForm propslist={propslist}/>
       <h2>Numbers</h2>
-        <ShowPersons persons={persons} />
+      <ShowPersons persons={persons} newFilter={newFilter} />
     </div>
-    
   )
 
 }
