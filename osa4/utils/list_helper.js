6f26c51430783a36c66800a53896e4632955b497
TobyQuality <topias.laatu@gmail.com>
2022-12-19 19:49:42 +0200

    part 4.a finished

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -44,6 +44,10 @@ const mostBlogs = (blogs) => {
 //credits belong to Mattias Petter Johansson and
 //his video on reduce method for making this possible
 const mostLikes = (blogs) => {
+    //the method reducer returns an object with
+    //name properties (of the authors) that have arrays.
+    //the arrays in turn have objects inside
+    //that store likes of each blogs.
     const reducer = blogs.reduce((bloggers, blog) => {
         bloggers[blog.author] = bloggers[blog.author] || []
         bloggers[blog.author].push({
@@ -51,7 +55,37 @@ const mostLikes = (blogs) => {
         })
         return bloggers
     }, {})
-    return reducer
+
+    //next the authors and arrays containing likes are put
+    //in their own separate arrays and their contents will be pushed
+    //in authorsAndLikes array in the for-loop
+    authors = Object.keys(reducer)
+    blogArrays = Object.values(reducer)
+    authorsAndLikes = []
+
+    for (i=0; i < authors.length; i++) {
+        likes = 0
+        blogArray = blogArrays[i]
+        for (j=0; j < blogArray.length; j++) {
+            likes += blogArray[j].likes
+        }
+        authorsAndLikes.push({'author': authors[i], 'likes': likes})
+    }
+
+    const likesOfTheMostLikedBlogger = Math.max(...authorsAndLikes.map((author) => author.likes))
+
+    //in the next for-loop each likes are compared to the most liked bloggers
+    //likes. If there is a match, an object containing properties' of the author's name
+    //and their likes is returned.
+    let authorWithMostLikes = {}
+    for (i=0; i < authorsAndLikes.length; i++) {
+        if (authorsAndLikes[i].likes === likesOfTheMostLikedBlogger) {
+            authorWithMostLikes = authorsAndLikes[i]
+            break
+        }
+    }
+
+    return authorWithMostLikes
 }
   
 module.exports = {
