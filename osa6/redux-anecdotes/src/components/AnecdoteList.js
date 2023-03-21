6546f9cdde16f648aacd5bad197a49b78bdd2d19
TobyQuality <topias.laatu@gmail.com>
2023-03-21 19:17:03 +0200

    6.9 valmis

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,15 +2,18 @@ import { useDispatch, useSelector } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => 
-        state.sort((a,b) => {return b.votes-a.votes}
+    const sortedAnecdotes = useSelector(state => 
+        state.anecdotes.sort((a,b) => {return b.votes-a.votes}
         ))
+    const filteredAnecdotes = useSelector(state =>
+        state.anecdotes.filter(anecdote => 
+            anecdote.content.toLowerCase().includes(state.filter)))
 
     const dispatch = useDispatch()
 
     return (
         <div>
-            {anecdotes.map(anecdote =>
+            {filteredAnecdotes.map(anecdote =>
                 <div key={anecdote.id}>
                     <div>
                         {anecdote.content}
